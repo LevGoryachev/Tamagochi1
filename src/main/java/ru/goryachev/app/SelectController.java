ab47f3df93f538aed09104d9df433a2d82e6cc37
Lev <l.goryachev@inbox.ru>
2020-03-11 22:29:10 +0300

    Разметка GUI

--- src/main/java/ru/goryachev/app/SelectController.java
+++ src/main/java/ru/goryachev/app/SelectController.java
@@ -1,4 +1,18 @@
 package ru.goryachev.app;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class SelectController {
+
+    @FXML
+    private void startHedgehog (ActionEvent event){
+        System.out.println("button Hedgehog");
+    }
+
+    @FXML
+    private void startCat (ActionEvent event){
+        System.out.println("button cat");
+    }
+
 }
